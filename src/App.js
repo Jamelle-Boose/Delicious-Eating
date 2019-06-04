e762ccefcb2f3c8c211f8852609da5d3c04fcd3b
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-06-04 10:24:45 -0700

    import edamam data

--- src/App.js
+++ src/App.js
@@ -6,22 +6,27 @@ const App = () => {
   const APP_ID = '1152003e'
   const APP_KEY = 'ccce8360d4b0c5e952da8ed25b3bc191'
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([])
 
-  const [counter, setCounter] = useState(0)
   useEffect(() => {
-    console.log(`Effect has been run`);
-  })
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    setRecipes(data.hits);
+  }
 
   return (
     <div className='App'>
       <form className='search-form'>
         <input className='search-bar' type='text' />
         <button className='search-button' type='submit'>
+          Search
         </button>
-        {counter}
       </form>
-    </div>
+    </div >
   );
 }
 
