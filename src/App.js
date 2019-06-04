d9e87b7166d30dd11926e320c671cd92d6c35d7c
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-06-04 09:51:02 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = '1152003e'
+  const APP_KEY = 'ccce8360d4b0c5e952da8ed25b3bc191'
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    console.log(`Effect has been run`);
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form className='search-form'>
+        <input className='search-bar' type='text' />
+        <button className='search-button' type='submit'>
+        </button>
+        {counter}
+      </form>
     </div>
   );
 }
